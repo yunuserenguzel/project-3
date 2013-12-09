dc354fe77f321655fe51ac3670586d16d23ff469
Yunus Eren Guzel <exculuber@gmail.com>
2013-12-09 23:50:12 +0200

    asdasd

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,4 @@
 
-Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-
-Paperclip::Attachment.default_options[:s3_host_name] = 'sonicraph.s3.amazonaws.com'
+#Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+#
+#Paperclip::Attachment.default_options[:s3_host_name] = 's3.amazonaws.com'
