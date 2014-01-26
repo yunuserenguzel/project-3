d91f8c4eaf9682fc61ae86127f3a289f3576ff31
Yunus Eren Guzel <exculuber@gmail.com>
2014-01-26 14:36:15 +0200

    asd

--- db/migrate/20140126121652_create_trigram_indexes.rb
+++ db/migrate/20140126121652_create_trigram_indexes.rb
@@ -1,6 +1,7 @@
 class CreateTrigramIndexes < ActiveRecord::Migration
   def change
-    execute "CREATE INDEX users_trgm_idx ON users USING gist (username gist_trgm_ops);"
-    execute "CREATE INDEX sonics_trgm_idx ON sonics USING gist (tags gist_trgm_ops);"
+    #execute "CREATE INDEX users_trgm_idx ON users USING gist (username gist_trgm_ops);"
+    #execute "CREATE INDEX sonics_trgm_idx ON sonics USING gist (tags gist_trgm_ops);"
+    #execute "CREATE INDEX users_trgm_idx ON users USING gin (username gin_trgm_ops)";
   end
 end
