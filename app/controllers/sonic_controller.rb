f12d86b45fb05648fb1ecbf08b4d51790d4d0c3e
Yunus Eren Guzel <exculuber@gmail.com>
2014-03-26 12:31:14 +0200

    asd

--- app/controllers/sonic_controller.rb
+++ app/controllers/sonic_controller.rb
@@ -5,7 +5,7 @@ class SonicController < ApplicationController
     @sonic = Sonic.where(:id=>params[:s]).first
     @isIphone = request.env['HTTP_USER_AGENT'].downcase.match(/iphone/)
     if @sonic
-      @iphoneUrl = "sonicraph://sonic/#{@sonic.id}%"
+      @iphoneUrl = "sonicraph://sonic/#{@sonic.id}"
     else
       @iphoneUrl = "sonicraph://open"
     end
