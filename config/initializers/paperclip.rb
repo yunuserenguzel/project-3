82ebcb68f95b800734de332b024e18021ce4057f
Yunus Eren Guzel <exculuber@gmail.com>
2014-04-11 10:25:53 +0300

    asd

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,4 @@
 
-Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-
-Paperclip::Attachment.default_options[:s3_host_name] = 's3-eu-west-1.amazonaws.com'
+#Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+#
+#Paperclip::Attachment.default_options[:s3_host_name] = 's3-website-us-east-1.amazonaws.com'
