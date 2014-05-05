3cb3ee94a9dcf1a23809a62571621aac1bd0d4b3
Yunus Eren Guzel <exculuber@gmail.com>
2014-05-05 21:50:59 +0300

    asd

--- app/controllers/sonic_controller.rb
+++ app/controllers/sonic_controller.rb
@@ -2,14 +2,28 @@ class SonicController < ApplicationController
 
 
   def index
-    @sonic = Sonic.where(:id=>params[:s]).first
-    @isIphone = request.env['HTTP_USER_AGENT'].downcase.match(/iphone/)
-    if @sonic
-      @iphoneUrl = "sonicraph://sonic/#{@sonic.id}"
+    if Rails.env.development?
+      @sonic = Object.new
+      @sonic.class.module_eval { attr_accessor :sonic_data, :user, :sonic_thumbnail, :tags}
+      @sonic.sonic_data = 's2312946224034h88e1b076baf1d2a44f9dbb62af36ab1ba5ec2703.snc'
+      user = @sonic.user = Object.new
+      user.class.module_eval {attr_accessor :profile_image_file_name, :profile_image, :fullname, :username, :location}
+      user.profile_image_file_name = 1
+      user.profile_image = 'u678243093894h2a34f611b93ec49a672deb22654fbceffbd8c4d9.jpg'
+      user.fullname = 'Yunus Eren Guzel'
+      user.username = 'yeguzel'
+      user.location = 'Ankara, TR'
     else
-      @iphoneUrl = "sonicraph://open"
+      @sonic = Sonic.where(:id=>params[:s]).first
+      @isIphone = request.env['HTTP_USER_AGENT'].downcase.match(/iphone/)
+      if @sonic
+        @iphoneUrl = "sonicraph://sonic/#{@sonic.id}"
+      else
+        @iphoneUrl = "sonicraph://open"
+      end
+      render :layout => false
+
     end
-    render :layout => false
   end
 
 end
